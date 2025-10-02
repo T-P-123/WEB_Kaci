7a1e2f7474367a4ce7d1e9323fc0c54e7b0c22ca
T-P-123 <tom.pavlicek123@gmail.com>
2025-10-02 15:54:48 +0200

    build: publish docs for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -6,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/WEB_Kaci/",                 // <<<<<<<<<< DŮLEŽITÉ pro GitHub Pages
+  base: "/WEB_Kaci/",            // <<< DŮLEŽITÉ pro GitHub Pages
   server: {
     host: "::",
     port: 8080,
